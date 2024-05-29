d37a5e8c7b8c5904ea4ec7d43d35f6d392b6f2f2
arthurkahwa <33757012+arthurkahwa@users.noreply.github.com>
2024-05-29 14:42:41 +0200

    Win conditions

--- ai_tic-tac-toe/ai_tic-tac-toe/ContentView.swift
+++ ai_tic-tac-toe/ai_tic-tac-toe/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var moves: [Move?] = Array(repeating: nil, count: 9)
-    @State private var isHumansTurn = true
+    @State private var isGameBoardDisabled = false
     
     var columns: [GridItem] = [GridItem(.flexible()),
                                GridItem(.flexible()),
@@ -33,19 +33,53 @@ struct ContentView: View {
                     .onTapGesture {
                         if isSquareOccupied(for: moves, atIndex: j) { return  }
                         
-                        moves[j] = .init(player: isHumansTurn ? .human : .computer,
-                                         boardIndex: j)
+                        moves[j] = .init(player: .human, boardIndex: j)
                         
-                        isHumansTurn.toggle()
+                        isGameBoardDisabled = true
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                            let computerPosition = determineComputerMovePosition(in: moves)
+                            
+                            moves[computerPosition] = .init(player: .computer,
+                                                            boardIndex: computerPosition)
+                            
+                            isGameBoardDisabled = false
+                            
+                        }
                     }
                 }
             }
         }
+        .disabled(isGameBoardDisabled)
         .padding()
     }
     
     func isSquareOccupied(for moves: [Move?], atIndex index: Int) -> Bool {
         return moves.contains(where: { $0?.boardIndex == index})
+    }
+    
+    func determineComputerMovePosition(in moves: [Move?]) -> Int {
+        var movePosition: Int = .random(in: 0..<9)
+        
+        while isSquareOccupied(for: moves, atIndex: movePosition) {
+            movePosition = .random(in: 0..<9)
+        }
+        
+        return movePosition
+    }
+    
+    func checkWinCondition(for player: Player, in moves: [Move?]) -> Bool {
+        let winPatterns: Set<Set<Int>> = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        
+        let playerMoves = moves
+            .compactMap { $0 }
+            .filter {  $0.player == player }
+        
+        let playerPositions = Set(playerMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns where pattern.isSubset(of: playerPositions) { return true }
+        
+        return false
     }
 }
 
