ee1d8f82078cb4f025cb917f8810e2d0f41c2e6e
arthurkahwa <33757012+arthurkahwa@users.noreply.github.com>
2024-05-31 14:45:46 +0200

    Use obsearvable syntax

--- ai_tic-tac-toe/ai_tic-tac-toe/GameView.swift
+++ ai_tic-tac-toe/ai_tic-tac-toe/GameView.swift
@@ -13,11 +13,46 @@ struct GameView: View {
     
     var body: some View {
         VStack {
+            Text("Scores:")
+                .font(.title2)
+                .fontWeight(.heavy)
+                .foregroundStyle(.secondary)
+                .padding(.top, 8)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Human")
+                        .foregroundStyle(.secondary)
+                    
+                    Spacer()
+                    
+                    Text("100")
+                }
+                
+                HStack {
+                    Text("Computer")
+                        .foregroundStyle(.secondary)
+                    
+                    Spacer()
+                    
+                    Text("100")
+                }
+            }
+            .font(.title)
+            .padding(.bottom, 64)
+            
+            
+            Text("Message")
+                .font(.callout)
+                .padding(.bottom, 24)
+            
+            
             LazyVGrid(columns: viewModel.columns, spacing: 4) {
                 ForEach(0..<9) { position in
                     ZStack {
                         Circle()
                             .foregroundStyle(.blue).opacity(0.8)
+                            .shadow(radius: 0.4)
  
                         Image(systemName: viewModel.moves[position]?.indicator ?? "")
                             .resizable()
@@ -29,6 +64,17 @@ struct GameView: View {
                     }
                 }
             }
+            .padding(.bottom, 64)
+            
+            Button(action: {
+                viewModel.resetGame()
+            }, label: {
+                Image(systemName: "restart.circle.fill")
+                    .resizable()
+                    .foregroundStyle(.orange)
+                    .frame(width: 84, height: 84)
+                    .buttonStyle(.borderedProminent)
+            })
         }
         .disabled(viewModel.isGameBoardDisabled)
         .padding()
